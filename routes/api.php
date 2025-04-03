879d1eb4c2710f9cf2dfd44f0dc58d1bd6b880a2
birukashagre6 <mandave693@gmail.com>
2025-04-03 14:37:36 +0300

    adding middleware to protect for protecting routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('posts', PostController::class);
 
-Route::post('register', [AuthController::class,'register']);
-Route::post('login', [AuthController::class,'login']);
-Route::post('logout', [AuthController::class,'logout']);
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+
 
 
