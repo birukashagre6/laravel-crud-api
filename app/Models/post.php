22c0f19b7a99efbe3b7d549604bb74be92b0fcb1
birukashagre6 <mandave693@gmail.com>
2025-04-04 23:02:33 +0300

    adding autherization for update and delete

--- app/Models/post.php
+++ app/Models/post.php
@@ -11,4 +11,8 @@ class post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'content'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
